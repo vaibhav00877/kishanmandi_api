023d416010d2ce5f7a58ea64ece015f8633f454f
vaibhav00877 <vaibhavtambat174@gmail.com>
2022-11-15 00:08:13 +0530

    setup

--- routers/index.js
+++ routers/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
+router.get("/",(req,res)=>{
+    res.send("All Good Kishan Bhaiyo...!")
+})
 router.use("/sellers", require("./sellers"));
 router.use("/customers", require("./customers"));
 router.use("/products", require("./products"));
